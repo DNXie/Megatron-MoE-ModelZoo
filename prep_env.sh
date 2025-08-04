68ed1de55e761d5aff218c18164d3d38f5e28a39
DNXie <dxie@meta.com>
2025-08-04 15:16:44 -0700

    script

--- prep_env.sh
+++
@@ -1,5 +0,0 @@
-export MEGATRON_PATH="/home/dxie/Megatron-LM"
-export MCORE_RELEASE_VERSION="0.14"
-# export CONTAINER_IMAGE="/path/to/container/image.sqsh"
-# export CLUSTER="your_cluster_name"
-# export TORCH_NCCL_AVOID_RECORD_STREAMS=0
